cf1426cc1229eb905f9149f1fe3f02a8c6b5076c
Eliza Mitchell <eliza.j.mitchell@gmail.com>
2018-03-21 22:41:42 -0400

    drop down added, not hooked up

--- planet_dataframe.py
+++ planet_dataframe.py
@@ -22,7 +22,6 @@ df[cols] = df[cols].replace({'%':'', '<':''}, regex = True)
 df[cols] = df[cols].apply(pd.to_numeric)
 
 app.layout = html.Div(children=[
-    html.H1(children='Planetary Atomospheres'),
     dcc.Graph(
         id = 'planet-atmo-scatter',
         figure = go.Figure(
@@ -46,8 +45,47 @@ app.layout = html.Div(children=[
                 margin=go.Margin(l=40, r=0, t=40, b=30)
             )
         )
-    )
+    ),
+
+    html.Div(children=[
+        dcc.Dropdown(
+            id='chem',
+            options=[{'label': i, 'value': i} for i in cols],
+            value='Carbon Dioxide'
+
+        ),
+        dcc.Graph(
+            id='chem-graph'
+        )
+    ])
 ])
+
+# TODO: Fix how the data is hooked up to the callback
+
+@app.callback(
+    dash.dependencies.Output('chem-graph', 'figure'),
+    [dash.dependencies.Input('chem', 'value')])
+def select_chem(chem):
+    return {
+        'data': [go.Bar(
+            x = df['Object'].loc[i],
+            y = df[chem].loc[i],
+            name = df['Object'].loc[i],
+            marker = go.Marker(
+                color = 'rgb({0}, {1}, {2})'.format(i * 30, i * 15, i * 5)
+            )
+        ) for i in range(0, 12)],
+        'layout': [go.Layout(
+            title='{}'.format(chem),
+            showlegend=True,
+            legend=go.Legend(
+                x=-0.5,
+                y=1.0
+            ),
+            margin=go.Margin(l=40, r=0, t=40, b=30)
+        )]
+
+    }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
